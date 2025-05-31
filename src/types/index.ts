d5839c5601ff893877150d92eb23786de2ef2117
lucianafs <luciana_ferreiras@outlook.com>
2025-05-31 12:58:49 -0300

    Removendo código não utilizado

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,34 +1,11 @@
 export interface Course {
   id: number;
   title: string;
+  courseTitle: string;
   description: string;
-  duration: string;
 }
 
 export interface FAQ {
   question: string;
   answer: string;
-}
-
-export interface NavbarLink {
-  label: string;
-  href: string;
-}
-
-export interface HeaderProps {
-  title: string;
-  subtitle: string;
-}
-
-export interface IntroProps {
-  imageUrl: string;
-  text: string;
-}
-
-export interface CoursesProps {
-  courses: Course[];
-}
-
-export interface FAQProps {
-  faqs: FAQ[];
 }
